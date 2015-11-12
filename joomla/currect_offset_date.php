7ad38e0276c8dcc133edca5af5b09c17a89f7571
Mansour Hussain Alnasser <alnassre.it@gmail.com>
2015-11-12 23:39:48 +0300

    Update currect_offset_date.php

--- joomla/currect_offset_date.php
+++ joomla/currect_offset_date.php
@@ -3,3 +3,7 @@
  *perfect date print with currect offset
  */
 echo JHTML::_('date', $receipt->created_date, JText::_('DATE_FORMAT_LC2'));
+
+
+// without time
+echo JHTML::_('date', $receipt->created_date, 'l, d F Y');
