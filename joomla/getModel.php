39d273d264a963f81439b9a32d942433c74bcdd5
Mansour Hussain Alnasser <alnassre.it@gmail.com>
2020-08-15 11:07:50 +0300

    Update getModel.php

--- joomla/getModel.php
+++ joomla/getModel.php
@@ -5,73 +5,20 @@ $ReceiptsModel = JModelLegacy::getInstance('Receipts', 'LlsModel');
 $query = $ReceiptsModel->getListQuery();
 
 
-// for other component
-/**
- * Webkul Software.
- *
- * @category  Webkul
- * @author    Webkul
- * @copyright Copyright (c) 2010-2016 Webkul Software Private Limited (https://webkul.com)
- * @license   https://store.webkul.com/license.html
- *
- *
- * getModelSite function
- *
- * @param [String] $component name of component
- * @param string $name name of model
- * @param string $prefix
- * @return Object
- */
-function getModelSite($component, $name = 'Custom', $prefix = 'CustomModel')
-{
-    if (!isset($component)) {
-        JFactory::getApplication()->enqueueMessage(JText::_("COM_ERROR_MSG"), 'error');
-        return false;
-    }
-    $path=JPATH_SITE . '/components/'.$component.'/models/';
-    JModelLegacy::addIncludePath($path);
-    require_once $path.strtolower($name).'.php';
-    $model = JModelLegacy::getInstance($name, $prefix);
-    // If the model is not loaded then $model will get false
-    if ($model == false) {
-        $class=$prefix.$name;
-        // initilize the model
-        new $class();
-        $model = JModelLegacy::getInstance($name, $prefix);
-    }
-    return $model;
-}
-/**
- * Webkul Software.
- *
- * @category  Webkul
- * @author    Webkul
- * @copyright Copyright (c) 2010-2016 Webkul Software Private Limited (https://webkul.com)
- * @license   https://store.webkul.com/license.html
- *
- * getModelAdmin function
- *
- * @param [String] $component name of component
- * @param string $name name of model
- * @param string $prefix
- * @return Object
- */
+//Admin Model:
 function getModelAdmin($component, $name = 'Custom', $prefix = 'CustomModel')
 {
-    if (!isset($component)) {
-        JFactory::getApplication()->enqueueMessage(JText::_("COM_ERROR_MSG"), 'error');
-        return false;
-    }
-    $path=JPATH_ADMINISTRATOR . '/components/'.$component.'/models/';
-    JModelLegacy::addIncludePath($path);
-    require_once $path.strtolower($name).'.php';
-    $model = JModelLegacy::getInstance($name, $prefix);
-    // If the model is not loaded then $model will get false
-    if ($model == false) {
-        $class=$prefix.$name;
-        // initilize the model
-        new $class();
-        $model = JModelLegacy::getInstance($name, $prefix);
-    }
+    JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/' . $component . '/tables', $prefix);
+    JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/' . $component . '/models', $prefix);
+    $model = JModelLegacy::getInstance($name, $prefix, array('ignore_request' => true));
+    return $model;
+}
+//Site Model:
+
+function getModelSite($component, $name = 'Custom', $prefix = 'CustomModel')
+{
+    JTable::addIncludePath(JPATH_SITE . '/components/' . $component . '/tables', $prefix);
+    JModelLegacy::addIncludePath(JPATH_SITE . '/components/' . $component . '/models', $prefix);
+    $model = JModelLegacy::getInstance($name, $prefix, array('ignore_request' => true));
     return $model;
 }
